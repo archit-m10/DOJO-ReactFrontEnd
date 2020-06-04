d21c4ea8c8ede0ce298ba4f95fa2f77c65765872
archit-m10 <archit_sharma@srmuniv.edu.in>
2020-06-04 13:13:15 +0530

    dojo

---
+++ src/pages/Home.js
@@ -0,0 +1,237 @@
+import React, {Component} from 'react';
+
+class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <nav class="navbar navbar-default navbar-fixed-top" data-spy="affix" data-offset-top="205">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand" href="#"><span class="s">DOJO &nbsp;</span></a> </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#about">partner@dojosports.in</a></li>
+                    <li><a href="#services">+91-9324648692</a></li>
+                    
+                    <li/><a href="#" class="btn btn-default-old">REGISTER NOW</a> 
+                </ul>
+            </div>
+        </div>
+    </nav>
+   {/* <!----- Details section  -----> */}
+    <section id="details_section">
+  
+    <div class="container-fluid details">
+    
+  <div class="row"> 
+      <div class="col-md-3 col-sm-6 number_Det">
+          <table align="center" class="details_table">
+            <tr>
+            <th><i class="fas fa-running fa-3x"></i></th>
+            <th><span class="no_count">09</span><br/><h5 class="details_h5">countries</h5></th>
+            </tr>
+          </table>
+      </div>
+      <div align="center" class="col-md-3 col-sm-6 number_Det">
+         <table class="details_table">
+            <tr>
+            <th><i class="fas fa-running fa-3x"></i></th>
+            <th><span class="no_count">1K+</span><br/><h5>customers</h5></th>
+            </tr>
+         </table>
+      </div>
+      <div class="col-md-3 col-sm-6 number_Det">
+          <table align="center" class="details_table">
+            <tr>
+            <th><i class="fas fa-running fa-3x"></i></th>
+            <th><span class="no_count">800+</span><br/><h5>DOJO Classe</h5></th>
+            </tr>
+          </table>
+      </div>
+      <div  class="col-md-3 col-sm-6 number_Det">
+            <table align="center" class="details_table">
+               <tr>
+               <th><i class="fas fa-running fa-3x"></i></th>
+               <th><span class="no_count">15+</span><br/><h5>Varies Sports</h5></th>
+               </tr>
+            </table>            
+       </div>
+      </div>
+    </div>
+</section>
+
+ 
+
+
+{/* <!--- Number details ---------> */}
+
+<section id="help">
+    <div class="container-fluid">
+  <div class="row">
+    <div class="col-md-6 build_h4">
+    <h2>Build to help everyone</h2><h2>Grow</h2>
+    <img class="help_pic" src="assets/images/asset-12.jpg" alt="help pic"/>
+    </div>
+    
+    <div class="col-md-6">
+       
+        <div class="col-sm-6 profile_det">  
+              <table>
+               <tr><th><img class="first" src="assets/images/profile.jpg" alt="profile pic"/></th></tr>
+               <tr><td><h4 class="profile_h4">Worldwide Network</h4><p class="profile_p">We Offer A Huge Vatiety Of Sports Classes In Over 23,000 Centres In More Than 800+Cities Around The World.</p></td></tr>
+             </table>
+        </div>
+
+        <div class="col-sm-6 profile_det">  
+              <table>
+               <tr><th><img class="first" src="assets/images/profile.jpg" alt="profile pic"/></th></tr>
+               <tr><td><h4 class="profile_h4">Worldwide Network</h4><p class="profile_p">We Offer A Huge Vatiety Of Sports Classes In Over 23,000 Centres In More Than 800+Cities Around The World.</p></td></tr>
+             </table>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="col-sm-6 profile_det">  
+                  <table>
+                   <tr><th><img class="first" src="assets/images/profile.jpg" alt="profile pic"/></th></tr>
+                   <tr><td><h4 class="profile_h4">Worldwide Network</h4><p class="profile_p">We Offer A Huge Vatiety Of Sports Classes In Over 23,000 Centres In More Than 800+Cities Around The World.</p></td></tr>
+                 </table>
+        </div>
+
+        <div class="col-sm-6 profile_det">  
+                <table>
+                 <tr><th><img class="first" src="assets/images/profile.jpg" alt="profile pic"/></th></tr>
+                 <tr><td><h4 class="profile_h4">Worldwide Network</h4><p class="profile_p">We Offer A Huge Vatiety Of Sports Classes In Over 23,000 Centres In More Than 800+Cities Around The World.</p></td></tr>
+               </table>
+        </div>
+      </div>
+  </div></div>
+</section> 
+
+{/* <!---- DOJO Partner  ----> */}
+<section id="partner">
+    <div class="container-fluid">
+ <div class="row">
+   <div class="col-md-12 partner_head">
+     <h2>Steps to Partner with DOJO</h2>
+     <p>Onboarding with DOJO is a hassle-free step process</p>
+   </div></div><div class="row"> 
+   <div class="col-md-4" >
+     <table  class="partner_table" >
+       <tr ><td align="center" ><img class="partner_with_oyo_step" src="assets/images/partner_with_oyo_step_1@2x.jpg" alt="profile pic"/></td></tr>
+       <tr><td align="center"><div class="numberCircle">1</div></td></tr>
+       <tr><td align="center"><p class="partner_p">Enter your details and we will call you to discuss the right solution for your asset</p></td></tr>
+     </table>
+    </div>
+   <div class="col-md-4">
+     <table class="partner_table">
+       <tr><td align="center"><img class="partner_with_oyo_step" src="assets/images/partner_with_oyo_step_2@2x.jpg" alt="profile pic"/></td></tr>
+       <tr><td align="center"><div class="numberCircle">2</div></td></tr>
+       <tr><td align="center"><p class="partner_p">Sign up with DOJO</p></td></tr>
+     </table>
+     </div>
+   <div class="col-md-4">
+      <table class="partner_table">
+          <tr><td align="center"><img class="partner_with_oyo_step" src="assets/images/partner_with_oyo_step_3@2x.jpg" alt="profile pic"/></td></tr>
+          <tr><td align="center"><div class="numberCircle">3</div></td></tr>
+          <tr><td align="center"><p class="partner_p">Onboard OYO’s technology suite for hassle-free operations</p></td></tr>
+        </table> 
+    </div>
+ </div></div>
+</section>
+
+{/* <!---- DOJO get ready  -----> */}
+<section id="get_ready">
+    <div class="container-fluid">
+  <div class="row">
+    <div class="col-md-6">
+        <img class="partner_join_us_world" src="assets/images/partner_join_us_world_v2.jpg" alt="partner join us world"/>
+    </div>
+   <div class="col-md-6 ready">
+     <div >
+     <h2>READY TO GET STARTED?</h2>
+     <p>Join the world’s fastest growing sports classes chain and unlock the potential of your assets! Request a call today and jump aboard the DOJO family.</p>
+    <button id="ready_call" >REGISTER NOW</button> 
+    </div></div>
+  </div></div>
+</section>
+
+
+    
+     
+    
+    <footer class="mainfooter" role="contentinfo">
+        <div class="footer-middle">
+        <div class="container">
+          <div class="row">
+            <li><span class="r">DOJO &nbsp;</span><a href="#">Find The Best Sports Class Near You</a></li>
+            <div class="col-md-3 col-sm-6">
+             {/*  <!--Column1--> */}
+              <div class="footer-pad">
+                
+                <ul class="list-unstyled">
+                  
+                  <li><a href="#"></a></li>
+                  <li><a href="#"></a></li><br /><br />
+                  <li><a href="#">About us</a></li>
+                  <li><a href="#">Team/Careers</a></li>
+                  <li><a href="#">Blog</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+             {/*  <!--Column1--> */}
+              <div class="footer-pad">
+                
+                <ul class="list-unstyled">
+                  <li><a href="#"></a></li>
+                  <li><a href="#"></a></li>
+                  <li><a href="#"></a></li><br /><br />
+                  <li><a href="#">Support</a></li>
+                  <li><a href="#">Official DOJO Blog</a></li>
+                  <li><a href="#">Press kit</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+             {/*  <!--Column1--> */}
+              <div class="footer-pad">
+                
+                <ul class="list-unstyled">
+                  <li><a href="#"></a></li>
+                  <li><a href="#"></a></li><br /><br />
+                  <li><a href="#">Privacy Policy</a></li>
+                  <li><a href="#">Quest Policy</a></li>
+                  <li><a href="#">Terms & Conditions</a></li>
+                  <li>
+                    <a href="#"></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+              <div class="col-md-3">
+                  <h4>Download The App</h4>
+                  <ul class="social-network social-circle">
+                   <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                   <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                  </ul>				
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-12 copy">
+                  <p class="text-center">2019 2020 &copy; DOJO Sports Private Limited</p>
+              </div>
+          </div>
+      
+      
+        </div>
+        </div>
+      </footer>
+        
+    
+    
+  
+            </div>
+        )
+    }
+}
+export default Home
